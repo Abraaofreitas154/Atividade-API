c8ba9bb81154d93d5340680f8af7798b454a7046
AbraãoFreitas-Dev <abraaofreitas154@gmail.com>
2026-03-26 21:06:48 -0300

    Versão inicial do jogo:Quem é esse Pokemon.

---
+++ script.js
@@ -0,0 +1,38 @@
+const container = document.getElementById('jogo-container');
+let nomeCorreto = "";
+
+async function novoDesafio() {
+    container.innerHTML = '<h2>Buscando Pokémon...</h2>';
+    const idSorteado = Math.floor(Math.random() * 151) + 1; // Sorteia da 1ª Geração
+
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${idSorteado}`);
+    const data = await res.json();
+    
+    nomeCorreto = data.name;
+    const imagemUrl = data.sprites.other['official-artwork'].front_default;
+
+    container.innerHTML = `
+        <div class="card-game">
+            <img src="${imagemUrl}" id="poke-img" class="silhueta">
+            <h2>Quem é esse Pokémon?</h2>
+            <input type="text" id="palpite" placeholder="Nome em inglês...">
+            <button onclick="verificar()">Revelar</button>
+            <br>
+            <button onclick="novoDesafio()" style="margin-top:10px; background:gray;">Pular</button>
+        </div>
+    `;
+}
+
+function verificar() {
+    const chute = document.getElementById('palpite').value.toLowerCase().trim();
+    const img = document.getElementById('poke-img');
+    
+    if (chute === nomeCorreto) {
+        img.classList.add('revelado');
+        alert("Acertou! Era o " + nomeCorreto.toUpperCase());
+    } else {
+        alert("Errou! Tente novamente.");
+    }
+}
+
+novoDesafio();
